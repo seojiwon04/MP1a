60cd935af09e37c4edae0f6ed867237c333f19af
seojiwon04 <g1seo2015@gmail.com>
2026-09-10 01:14:47 -0500

    start

---
+++ Assets/Scripts/ObjectShoot.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject star;
+    public ParticleSystem poof;
+    public Camera mainCamera;
+    public InputActionReference spawn;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
