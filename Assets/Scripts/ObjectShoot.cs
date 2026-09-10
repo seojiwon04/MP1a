fcebee9974675f01f01713781cea167595355646
seojiwon04 <g1seo2015@gmail.com>
2026-09-10 02:16:16 -0500

    spawn debugging

--- Assets/Scripts/ObjectShoot.cs
+++ Assets/Scripts/ObjectShoot.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ObjectSpawn : MonoBehaviour
+public class ObjectShoot : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject star;
